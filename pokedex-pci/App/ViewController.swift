45928d7cb5fd02cdceb50d61a1c1a23e146897d9
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2025-03-20 18:36:33 -0300

    Create the project, setup packages and show a simple ViewController programatically

---
+++ pokedex-pci/App/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  pokedex-pci
+//
+//  Created by Manny Alvarez on 20/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
