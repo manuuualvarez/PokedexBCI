dbc589b437ebff907841ef027727546564ccf54e
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2025-03-20 18:24:56 -0300

    Initial Commit

---
+++ pokedex-pci/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokedex-pci
+//
+//  Created by Manny Alvarez on 20/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pokedex-pciTests/pokedex_pciTests.swift
@@ -0,0 +1,17 @@
+//
+//  pokedex_pciTests.swift
+//  pokedex-pciTests
+//
+//  Created by Manny Alvarez on 20/03/2025.
+//
+
+import Testing
+@testable import pokedex_pci
+
+struct pokedex_pciTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
